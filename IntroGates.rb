1eb63612310c3251096696ce48f405618d720c4a
Carlos Cuenca <cuenca.cl@gmail.com>
2020-07-06 11:46:23 -0700

    implement addTwoNumbers

---
+++ IntroGates.rb
@@ -0,0 +1,13 @@
+# --------------------------------------------
+# Returns the sum of the given number's digits
+#
+# Problem #1
+#
+# @author: Carlos L. Cuenca
+# @date 07/06/2020
+
+
+def addTwoDigits(n)
+	return (n / 10) + (n % 10)
+end
+
